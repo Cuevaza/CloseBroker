5c880fb24cdce68147cf755a632b83d439f4e0f2
Cuevaza <viejoviscaino@gmail.com>
2023-12-07 17:39:34 -0600

    commit base... primero

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+import config
+import os
+import csv
+from binance.client import Client
+from datetime import datetime
+
+# Crear cliente de Binance
+api_key = config.api_key
+api_secret = config.api_secret
